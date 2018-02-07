0142eaa87fe88ac085269a45173ea71c20392777
neifirst <e.mcculloch.arch@gmail.com>
2018-02-07 14:34:11 +0000

    added artist and album classes

---
+++ models/album.rb
@@ -0,0 +1,17 @@
+require('pg')
+require_relative('./artist')
+# require_relative('../db/sql_runner')
+
+class Album
+
+  attr_accessor :title, :genre, :artist_id
+  attr_reader :id
+
+  def initialize(options)
+
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['artist_id'].to_i
+
+  end
